a09148595f4ff1764b889a53d11229721e657459
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-13 15:18:33 +0100

    aggiunge scaffolding

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page-title')
+Homepage
+@endsection
+
+
+@section("page-content")
+
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="my-container d-flex j-content-sb">
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li class="active"><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
