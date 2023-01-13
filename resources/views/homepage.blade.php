1e0fb319a2fb34587758fcaf17dc5166d13ce776
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-13 16:11:20 +0100

    aggiunge file con info comics

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,22 @@ Homepage
 
 
 @section("page-content")
-
+<section class="my-container">
+    <div class="comics">
+        <button>
+            <h2>CURRENT SERIES</h2>
+        </button>
+        <div class="comic" >
+            <img src="" alt="">
+            <h5></h5>
+        </div>
+        
+    </div>
+    <div class="d-flex">
+        <button class="my-secondary-btn">
+            LOAD MORE
+        </button>
+    </div>
+</section>
 
 @endsection
