11313b8504bd55be4a57ca5ce3b581324e4bed6f
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-13 16:51:19 +0100

    aggiunge bg alla comic

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,28 +6,31 @@ Homepage
 
 
 @section("page-content")
-<section class="my-container">
-    
-    <div class="comics">
-        <button>
-            <h2>CURRENT SERIES</h2>
-        </button>
+<div class="my-bg">
 
+
+    <section class="my-container">
         
-        @foreach ($comics as $comic)
-        <div class="comic" >
-            <img src="{{$comic['thumb']}}" alt="">
-            <h5>{{$comic['series']}}</h5>
-        </div>
-        @endforeach
+        <div class="comics">
+            <button>
+                <h2>CURRENT SERIES</h2>
+            </button>
+
             
-        
-    </div>
-    <div class="d-flex">
-        <button class="my-secondary-btn">
-            LOAD MORE
-        </button>
-    </div>
-</section>
-
+            @foreach ($comics as $comic)
+            <div class="comic" >
+                <img src="{{$comic['thumb']}}" alt="">
+                <h5>{{$comic['series']}}</h5>
+            </div>
+            @endforeach
+                
+            
+        </div>
+        <div class="d-flex">
+            <button class="my-secondary-btn">
+                LOAD MORE
+            </button>
+        </div>
+    </section>
+</div>
 @endsection
