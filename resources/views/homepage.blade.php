d11875af90bbd15a367324f09f3b3fe5b36461a7
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-13 16:33:10 +0100

    aggiunge fumetti in modo dinamico

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,14 +7,20 @@ Homepage
 
 @section("page-content")
 <section class="my-container">
+    
     <div class="comics">
         <button>
             <h2>CURRENT SERIES</h2>
         </button>
+
+        
+        @foreach ($comics as $comic)
         <div class="comic" >
-            <img src="" alt="">
-            <h5></h5>
+            <img src="{{$comic['thumb']}}" alt="">
+            <h5>{{$comic['series']}}</h5>
         </div>
+        @endforeach
+            
         
     </div>
     <div class="d-flex">
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comics = config("comics");
-    dd($comics);
-    return view('homepage');
+   
+    return view('homepage' , compact("comics"));
 });
